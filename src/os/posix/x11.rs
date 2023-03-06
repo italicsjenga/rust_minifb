467ad2666fe76c65e1615d659528c4f4b2d2af08
Alex Janka <alex@alexjanka.com>
2023-03-06 12:28:45 +1100

    posix displayhandle

--- src/os/posix/x11.rs
+++ src/os/posix/x11.rs
@@ -334,8 +334,16 @@ unsafe impl raw_window_handle::HasRawWindowHandle for Window {
     fn raw_window_handle(&self) -> raw_window_handle::RawWindowHandle {
         let mut handle = raw_window_handle::XlibWindowHandle::empty();
         handle.window = self.handle;
-        handle.display = self.d.display as *mut core::ffi::c_void;
         raw_window_handle::RawWindowHandle::Xlib(handle)
+    }
+}
+
+unsafe impl HasRawDisplayHandle for Window {
+    fn raw_display_handle(&self) -> raw_window_handle::RawDisplayHandle {
+        let mut handle = raw_window_handle::XlibDisplayHandle::empty();
+        handle.display = self.d.display as *mut core::ffi::c_void;
+
+        RawDisplayHandle::Xlib(handle)
     }
 }
 
